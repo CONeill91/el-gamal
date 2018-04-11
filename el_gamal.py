909b8e683b8de7001c465793d59209b206d0a516
Conor O'Neill <coneill@wartek.com>
2018-04-11 19:02:47 +0100

    initial commit

---
+++ el_gamal.py
@@ -0,0 +1,15 @@
+def order(p, f, a):
+    """
+    Practical 1:
+    order(p, f, a) will  return  the  order of a in  the  group Z∗p
+    where: f is  a  list  of  the  prime  factors of p - 1
+    Ensure  that  your  method  works  when f contains  duplicates.
+    """
+
+
+def findg(p, f):
+    """
+    Practical 2:
+    find(p, f) will return a generator of Z*p where p is a prime and f is a list of the prime factors of p - 1
+    """
+    
